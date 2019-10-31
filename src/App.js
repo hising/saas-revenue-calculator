a24066eee8c1905636aff0b5a54039d06c1d131b
Mattias Hising <hising@gmail.com>
2019-10-31 20:07:07 +0100

    Added header and h1 and shit

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import { observer } from "mobx-react";
 import { Tabs } from "./components/Tabs";
 import { Changes } from "./components/Changes";
 import ReactGA from "react-ga";
+import { Header } from "./components/Header";
 
 const revenueStore = new RevenueStore();
 
@@ -51,189 +52,210 @@ class App extends Component {
     });
     return (
       <div className="App">
-        <div className="row">
-          <div className="col-lg-8">
-            <Tabs
-              items={[{ key: "users", label: "Users" }, { key: "income", label: "Income" }]}
-              onChange={(key) => {
-                this.setState({
-                  active: key
-                });
-              }}
-              active={this.state.active}
-            />
-            <div className={this.state.active === "users" ? "tab-show" : "tab-hidden"}>
-              <Chart data={revenueStore.data} />
-            </div>
-            <div className={this.state.active === "income" ? "tab-show" : "tab-hidden"}>
-              <Chart data={revenueStore.getIncomeData} />
-            </div>
-            <HistoricTable
-              data={revenueStore.data}
-              money={{
-                reactivationCost: revenueStore.reactivationCost,
-                acquisitionCost: revenueStore.acquisitionCost,
-                monthlyRevenuePerUser: revenueStore.income
-              }}
-            />
-          </div>
-          <div className="col-lg-4">
-            <div className="card">
-              <div className="card-body">
-                <h5 className="card-title">Total Income after {revenueStore.months} months</h5>
-                <p className="card-subtitle mb-2 text-muted">
-                  <small>
-                    ${revenueStore.income}/user/month - MoM Acq Growth: {toPercentage(revenueStore.growthFactor)} -
-                    Churn-rate: {toPercentage(revenueStore.monthlyChurn)} - Reactivation:{" "}
-                    {toPercentage(revenueStore.reactivationRate)} - Acquisition Cost: ${revenueStore.acqusitionCost} -
-                    Reactivation Cost: ${revenueStore.reactivationCost}
-                  </small>
-                </p>
-                <p className={"total-income"}>{this.showMoney(revenueStore.totalIncome, 0)}</p>
-                <h6>What would happen if we would...</h6>
-                <dl className={"total-insights"}>
-                  <dt>Increase nr of new users with 5%</dt>
-                  <dd>
-                    <Changes data={monthlies} />
-                  </dd>
-
-                  <dt>Increase growth factor with 1%</dt>
-                  <dd>
-                    <Changes data={growhties} />
-                  </dd>
-
-                  <dt>Reduce churn rate with 1%</dt>
-                  <dd>
-                    <Changes data={churnies} />
-                  </dd>
-
-                  <dt>Increase reactivation rate with 1%</dt>
-                  <dd>
-                    <Changes data={recRaties} />
-                  </dd>
-
-                  <dt>Increase income per user with 1%</dt>
-                  <dd>
-                    <Changes data={incomies} />
-                  </dd>
-
-                  <dt>Reduce acquisition cost with 1%</dt>
-                  <dd>
-                    <Changes data={acquies} />
-                  </dd>
-
-                  <dt>Reduce reactivation cost with 1%</dt>
-                  <dd>
-                    <Changes data={reccies} />
-                  </dd>
-                </dl>
+        <Header>
+          SaaS Revenue Calculator by Yetric AB
+          <a
+            href={"#share"}
+            onClick={(event) => {
+              event.preventDefault();
+              alert("not done yet, copy the url and share!");
+            }}
+            className={"action"}>
+            Share this Chart!
+          </a>
+        </Header>
+        <div className="main-content">
+          <h1 className={"h2"}>
+            SaaS Revenue Calculator <small className={"text-muted"}>by Yetric AB</small>
+          </h1>
+          <h2 className={"h5 mb-3"}>
+            <small className={"text-muted"}>How will different metrics effect your bottom line?</small>
+          </h2>
+          <div className="row">
+            <div className="col-lg-8">
+              <Tabs
+                items={[{ key: "users", label: "Users" }, { key: "income", label: "Income" }]}
+                onChange={(key) => {
+                  this.setState({
+                    active: key
+                  });
+                }}
+                active={this.state.active}
+              />
+              <div className={this.state.active === "users" ? "tab-show" : "tab-hidden"}>
+                <Chart data={revenueStore.data} />
               </div>
+              <div className={this.state.active === "income" ? "tab-show" : "tab-hidden"}>
+                <Chart data={revenueStore.getIncomeData} />
+              </div>
+              <HistoricTable
+                data={revenueStore.data}
+                money={{
+                  reactivationCost: revenueStore.reactivationCost,
+                  acquisitionCost: revenueStore.acquisitionCost,
+                  monthlyRevenuePerUser: revenueStore.income
+                }}
+              />
             </div>
-            <Slider
-              label={"First Month New Customers"}
-              value={revenueStore.monthlyNew}
-              onChange={(event) => {
-                revenueStore.setMonthlyNew(toInt(event.target.value));
-              }}
-              refName={"monthlyNewRef"}
-              step={5}
-              min={0}
-              max={100}
-              info="TODO: Add possibility to start with base number"
-            />
-            <Slider
-              label={"Monthly Growth Factor"}
-              value={revenueStore.growthFactor}
-              onChange={(event) => {
-                revenueStore.setGrowthFactor(toFloat(event.target.value));
-              }}
-              refName={"growthFactorRef"}
-              step={0.01}
-              min={-0.5}
-              max={0.5}
-              formatter={percentageFormatter}
-              info="TODO: Decay over time as optional"
-            />
-            <Slider
-              label={"Monthly Churn Rate"}
-              value={revenueStore.monthlyChurn}
-              onChange={(event) => {
-                revenueStore.setMonthlyChurn(toFloat(event.target.value));
-              }}
-              refName={"churnFactorRef"}
-              step={0.01}
-              min={0}
-              max={0.5}
-              formatter={percentageFormatter}
-              info="TODO: Decay over time as optional"
-            />
+            <div className="col-lg-4">
+              <div className="card">
+                <div className="card-body">
+                  <h5 className="card-title">Total Income after {revenueStore.months} months</h5>
+                  <p className="card-subtitle mb-2 text-muted">
+                    <small>
+                      ${revenueStore.income}/user/month - MoM Acq Growth: {toPercentage(revenueStore.growthFactor)} -
+                      Churn-rate: {toPercentage(revenueStore.monthlyChurn)} - Reactivation:{" "}
+                      {toPercentage(revenueStore.reactivationRate)} - Acquisition Cost: ${revenueStore.acqusitionCost} -
+                      Reactivation Cost: ${revenueStore.reactivationCost}
+                    </small>
+                  </p>
+                  <p className={"total-income"}>{this.showMoney(revenueStore.totalIncome, 0)}</p>
+                  <h6>What would happen if we would...</h6>
+                  <dl className={"total-insights"}>
+                    <dt>Increase nr of new users with 5%</dt>
+                    <dd>
+                      <Changes data={monthlies} />
+                    </dd>
 
-            <Slider
-              label={"Reactivation Rate"}
-              value={revenueStore.reactivationRate}
-              onChange={(event) => {
-                revenueStore.setReactivationRate(toFloat(event.target.value));
-              }}
-              refName={"reactivationRateRef"}
-              step={0.01}
-              min={0}
-              max={0.99}
-              formatter={percentageFormatter}
-              info="TODO: Decay or improve over time as optional"
-            />
+                    <dt>Increase growth factor with 1%</dt>
+                    <dd>
+                      <Changes data={growhties} />
+                    </dd>
 
-            <Slider
-              label={"Months"}
-              value={revenueStore.months}
-              onChange={(event) => {
-                revenueStore.setMonths(toInt(event.target.value));
-              }}
-              refName={"changeMonthsRef"}
-              step={12}
-              min={0}
-              max={120}
-            />
+                    <dt>Reduce churn rate with 1%</dt>
+                    <dd>
+                      <Changes data={churnies} />
+                    </dd>
 
-            <Slider
-              label={"Monthly Income Per User"}
-              value={revenueStore.income}
-              onChange={(event) => {
-                revenueStore.setIncome(toInt(event.target.value));
-              }}
-              refName={"changeIncomeRef"}
-              step={1}
-              min={1}
-              max={200}
-              info="TODO: Add a function to increase this over time"
-            />
+                    <dt>Increase reactivation rate with 1%</dt>
+                    <dd>
+                      <Changes data={recRaties} />
+                    </dd>
 
-            <Slider
-              label={"Acquisition Cost"}
-              value={revenueStore.acquisitionCost}
-              onChange={(event) => {
-                revenueStore.setAcquisitionCost(toInt(event.target.value));
-              }}
-              refName={"changeAcqusitionCostRef"}
-              step={5}
-              min={0}
-              max={200}
-              info="TODO: Lower over time as optional"
-            />
+                    <dt>Increase income per user with 1%</dt>
+                    <dd>
+                      <Changes data={incomies} />
+                    </dd>
 
-            <Slider
-              label={"Reactivation Cost"}
-              value={revenueStore.reactivationCost}
-              onChange={(event) => {
-                revenueStore.setReactivationCost(toInt(event.target.value));
-              }}
-              refName={"changeReactivationCostRef"}
-              step={5}
-              min={0}
-              max={200}
-              info="TODO: Lower over time as optional"
-            />
+                    <dt>Reduce acquisition cost with 1%</dt>
+                    <dd>
+                      <Changes data={acquies} />
+                    </dd>
+
+                    <dt>Reduce reactivation cost with 1%</dt>
+                    <dd>
+                      <Changes data={reccies} />
+                    </dd>
+                  </dl>
+                </div>
+              </div>
+              <Slider
+                label={"First Month New Customers"}
+                value={revenueStore.monthlyNew}
+                onChange={(event) => {
+                  revenueStore.setMonthlyNew(toInt(event.target.value));
+                }}
+                refName={"monthlyNewRef"}
+                step={5}
+                min={0}
+                max={100}
+                info="TODO: Add possibility to start with base number"
+              />
+              <Slider
+                label={"Monthly Growth Factor"}
+                value={revenueStore.growthFactor}
+                onChange={(event) => {
+                  revenueStore.setGrowthFactor(toFloat(event.target.value));
+                }}
+                refName={"growthFactorRef"}
+                step={0.01}
+                min={-0.5}
+                max={0.5}
+                formatter={percentageFormatter}
+                info="TODO: Decay over time as optional"
+              />
+              <Slider
+                label={"Monthly Churn Rate"}
+                value={revenueStore.monthlyChurn}
+                onChange={(event) => {
+                  revenueStore.setMonthlyChurn(toFloat(event.target.value));
+                }}
+                refName={"churnFactorRef"}
+                step={0.01}
+                min={0}
+                max={0.5}
+                formatter={percentageFormatter}
+                info="TODO: Decay over time as optional"
+              />
+
+              <Slider
+                label={"Reactivation Rate"}
+                value={revenueStore.reactivationRate}
+                onChange={(event) => {
+                  revenueStore.setReactivationRate(toFloat(event.target.value));
+                }}
+                refName={"reactivationRateRef"}
+                step={0.01}
+                min={0}
+                max={0.99}
+                formatter={percentageFormatter}
+                info="TODO: Decay or improve over time as optional"
+              />
+
+              <Slider
+                label={"Months"}
+                value={revenueStore.months}
+                onChange={(event) => {
+                  revenueStore.setMonths(toInt(event.target.value));
+                }}
+                refName={"changeMonthsRef"}
+                step={12}
+                min={0}
+                max={120}
+              />
+
+              <Slider
+                label={"Monthly Income Per User"}
+                value={revenueStore.income}
+                onChange={(event) => {
+                  revenueStore.setIncome(toInt(event.target.value));
+                }}
+                refName={"changeIncomeRef"}
+                step={1}
+                min={1}
+                max={200}
+                info="TODO: Add a function to increase this over time"
+              />
+
+              <Slider
+                label={"Acquisition Cost"}
+                value={revenueStore.acquisitionCost}
+                onChange={(event) => {
+                  revenueStore.setAcquisitionCost(toInt(event.target.value));
+                }}
+                refName={"changeAcqusitionCostRef"}
+                step={5}
+                min={0}
+                max={200}
+                info="TODO: Lower over time as optional"
+              />
+
+              <Slider
+                label={"Reactivation Cost"}
+                value={revenueStore.reactivationCost}
+                onChange={(event) => {
+                  revenueStore.setReactivationCost(toInt(event.target.value));
+                }}
+                refName={"changeReactivationCostRef"}
+                step={5}
+                min={0}
+                max={200}
+                info="TODO: Lower over time as optional"
+              />
+            </div>
           </div>
         </div>
+
         <div className={"mt-3 mb-3 text-center text-muted"}>
           Built by <a href={"https://yetric.com"}>Yetric AB</a>
         </div>
